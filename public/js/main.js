ff16bf76e1d933a3da69360f4f24871af4c800be
Angel Contreras <acontrerasrosario@gmail.com>
2017-03-04 14:46:43 -0400

    Iniciando proyecto APIDAE

---
+++ public/js/main.js
@@ -0,0 +1,14 @@
+
+
+<script src="https://www.gstatic.com/firebasejs/3.7.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB0IXn8A10yGVM5rGR871L7i2BapIOMgQQ",
+    authDomain: "apidae-4f97d.firebaseapp.com",
+    databaseURL: "https://apidae-4f97d.firebaseio.com",
+    storageBucket: "apidae-4f97d.appspot.com",
+    messagingSenderId: "841472032050"
+  };
+  firebase.initializeApp(config);
+</script>
